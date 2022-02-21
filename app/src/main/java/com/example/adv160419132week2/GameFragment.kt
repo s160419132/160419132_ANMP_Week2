03e27d811888c0699f1892b4293cab500d8ce6dd
Kevin sihombing <s160419132@student.ubaya.ac.id>
2022-02-21 20:46:14 +0700

    commit project

---
+++ app/src/main/java/com/example/adv160419132week2/GameFragment.kt
@@ -0,0 +1,60 @@
+package com.example.adv160419132week2
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_game.*
+
+class GameFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(arguments != null){
+            val playerName= GameFragmentArgs.fromBundle(requireArguments()).playerName
+            txtTurn.text= "$playerName's Turn"
+        }
+        var score =0
+        var A= (0..100).random()
+        var B= (0..100).random()
+        Log.d('A'.toString(), A.toString())
+        var C= A+B
+        Log.d('C'.toString(), C.toString())
+        txtBilA.setText(A.toString())
+        txtBilB.setText(B.toString())
+        btnSubmit.setOnClickListener{
+            var answer= txtAnswer.text.toString()
+            if(answer==C.toString()){
+                var X= (0..100).random()
+                var Y= (0..100).random()
+                C= X+Y
+                txtBilA.setText(X.toString())
+                txtBilB.setText(Y.toString())
+                score=score+1
+                Log.d("score",score.toString())
+                txtAnswer.setText("")
+            }
+            else{
+                val totalscore= score.toString()
+                val action= GameFragmentDirections.actionResultFragment(totalscore)
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
+ /*       btnBack.setOnClickListener{
+            val action= GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }*/
+    }
+}
---
+++ app/src/main/java/com/example/adv160419132week2/ResultFragment.kt
@@ -0,0 +1,32 @@
+package com.example.adv160419132week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_result.*
+
+
+class ResultFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val score =ResultFragmentArgs.fromBundle(requireArguments()).score
+        txtScore.setText("Your Score is $score")
+        btnBack.setOnClickListener {
+            val action= ResultFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
